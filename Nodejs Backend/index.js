e0a26a930da91f5f455d5e690b25443e531413dc
Collins Kipruto <colllinskandie1@gmail.com>
2022-05-30 19:48:11 +0300

    React base code updated

--- Nodejs Backend/index.js
+++ Nodejs Backend/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app =express();
+app.use(express.json);
+const courses =[
+    {id: 1, name:"course1"},
+    {id: 2,name:"course2"},
+];
 
 app.get('/', (req,res)=>{
     res.send("Hi Collins, You are writing your first backend code");
 })
 
-app.listen(3001, ()=> console.log("port 3001 active"))
+app.post('/api/courses', (req,res)=>{
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name 
+    }
+    courses.push(course);
+    res.send(course); 
+})
+//set the port of the app
+const port =process.env.PORT || 3000
+app.listen(port, ()=> console.log(port, "port  active"));
