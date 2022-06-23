54a813ad13483b9b7e1a637f49f220aa229856b8
Collins Kipruto <colllinskandie1@gmail.com>
2022-06-23 16:49:39 +0300

    data

--- front-end/src/Pages/Salesperson.jsx
+++ front-end/src/Pages/Salesperson.jsx
@@ -5,37 +5,26 @@ import Sidebar from "../Components/sidebar";
 import Navbar from "../Components/Navbar";
 import { DataGrid } from "@mui/x-data-grid";
 const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "ProductClass", headerName: "Product", width: 70 },  
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
+    field: "total",
+    headerName: "Total Sales",
+    description: "This column has a value getter and is not sortable.",    
     width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
   },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+// const rows = [
+//   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+//   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
 function Salesperson() {
   const [data, setData] = useState(null);
   const URL1 = "/api/salesperson";
@@ -69,12 +58,12 @@ function Salesperson() {
           <label>Name:</label>
           <label>Total Sales: </label>
           <div>
-            <div style={{ height: 400, width: "100%" }}>
+            <div style={{ height: 700, width: "100%" }}>
               <DataGrid
                 rows={data}
                 columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}                
+                // pageSize={5}
+                rowsPerPageOptions={[1000]}                
               />
             </div>
           </div>
